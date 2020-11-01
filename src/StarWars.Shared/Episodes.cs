99b0a05713aab2b283c2d2b5ff4936ed86ee3150
Hubert Krynicki <krynicki.hubert@gmail.com>
2020-11-01 11:03:52 +0100

    Added missing shared project

---
+++ src/StarWars.Shared/Episodes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace StarWars.Shared
+{
+    public static class Episodes
+    {
+        //Original trilogy names
+        public static readonly string[] Names = { "NEWHOPE", "EMPIRE", "JEDI" };
+        public static bool IsValid(string name) => Names.Contains(name);
+    }
+}
